0a243940ff88eb07efe15558e1de000c4f0af27a
duanxing <shensiduanxing@gmail.com>
2015-02-25 01:12:10 +0800

    ini

---
+++ bin/start.sh
@@ -0,0 +1,7 @@
+
+
+#start memcache
+MEMCACHE_HOME=Downloads/Middlewares/memcached-1.4.20
+$MEMCACHE_HOME/memcached -d -m 64 -l 127.0.0.1 -p 11211
+
+Downloads/Middlewares/rabbitmq_server-2.8.7/sbin/rabbitmq-server
